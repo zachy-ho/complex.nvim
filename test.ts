39b5b28cc349e8514252e2a574eb75a0481d3b06
zachy-ho <zachyho@canva.com>
2023-11-07 23:03:43 +1100

    test file

---
+++ test.ts
@@ -0,0 +1,19 @@
+function myFunc() {
+  console.log('hello')
+}
+
+function func2() {
+  console.log('hello')
+  function inner() {
+    console.log('inner')
+    function innnnn() {
+
+    }
+  }
+}
+
+const papi = ''
+
+const huh = () => {
+  console.log('do this')
+}
