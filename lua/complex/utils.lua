a9d4b82f49a3e7211c2d0a69ddcf6e809c60435d
zachy-ho <zachyho@canva.com>
2024-01-30 23:54:02 +1100

    added table concat util function

--- lua/complex/utils.lua
+++ lua/complex/utils.lua
@@ -22,4 +22,19 @@ function M.size(t)
 	return count
 end
 
+-- Returns a new list with table2 concatenated on table1
+---@param table1 table keyed table
+---@param table2 table keyed table
+---@return table merged { ...table1, ...table2 }
+function M.concat(table1, table2)
+	local merged = {}
+	for _, v in ipairs(table1) do
+		table.insert(merged, v)
+	end
+	for _, v in ipairs(table2) do
+		table.insert(merged, v)
+	end
+	return merged
+end
+
 return M
